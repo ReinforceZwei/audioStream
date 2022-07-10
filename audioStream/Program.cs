2a9db30f3d9939fa8d7515bcff0750e5630f7c67
ReinforceZwei <39955851+ReinforceZwei@users.noreply.github.com>
2022-07-10 16:00:36 +0800

    Clean up the code

--- audioStream/Program.cs
+++ audioStream/Program.cs
@@ -1,4 +1,5 @@
 ﻿using NAudio.Wave;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -7,60 +8,34 @@ namespace audioStream
 {
     internal class Program
     {
-        static MemoryStream audioBuf = new();
-        static object audioStreamLock = new object();
-        static AutoResetEvent audioDataEvent = new(true);
-
-        static Stream clientS;
+        static Collection<Stream> clientS = new Collection<Stream>();
 
         static void Main(string[] args)
         {
-            var outputFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "NAudio");
-            Directory.CreateDirectory(outputFolder);
-            var outputFilePath = Path.Combine("S:\\", "recorded.wav");
             var capture = new WasapiLoopbackCapture();
-            // optionally we can set the capture waveformat here: e.g. capture.WaveFormat = new WaveFormat(44100, 16,2);
-            //var writer = new WaveFileWriter(outputFilePath, capture.WaveFormat);
-            //var rawWriter = new FileStream("S:\\raw.wav", FileMode.Create);
-            Console.WriteLine(capture.WaveFormat.SampleRate);
-            Console.WriteLine(capture.WaveFormat.Channels);
-            Console.WriteLine(capture.WaveFormat.BitsPerSample);
             capture.DataAvailable += (s, a) =>
             {
-                //writer.Write(a.Buffer, 0, a.BytesRecorded);
-                //rawWriter.Write(a.Buffer, 0, a.BytesRecorded);
-                //Console.WriteLine("[WAV] Got " + a.BytesRecorded);
-                lock (audioStreamLock)
+                if (clientS.Count > 0)
                 {
-                    if (clientS != null)
+                    // Reverse looping
+                    for (int i = clientS.Count - 1; i >= 0; i--)
                     {
-                        clientS.Write(a.Buffer, 0, a.BytesRecorded);
+                        try
+                        {
+                            clientS[i].Write(a.Buffer, 0, a.BytesRecorded);
+                        }
+                        catch
+                        {
+                            clientS[i].Dispose();
+                            clientS.RemoveAt(i);
+                            Console.WriteLine("[STREAM] Disposed one client stream");
+                        }
                     }
-                    audioBuf = new MemoryStream(a.BytesRecorded);
-                    audioBuf.Write(a.Buffer, 0, a.BytesRecorded);
-                    audioDataEvent.Set();
-                    audioDataEvent.Reset();
                 }
-                //if (writer.Position > capture.WaveFormat.AverageBytesPerSecond * 10)
-                //{
-                //    capture.StopRecording();
-                //}
             };
-
-            capture.RecordingStopped += (s, a) =>
-            {
-                //writer.Dispose();
-                //writer = null;
-                capture.Dispose();
-            };
-
-            new Thread(() => { RunServer(); }).Start();
 
             capture.StartRecording();
-            while (capture.CaptureState != NAudio.CoreAudioApi.CaptureState.Stopped)
-            {
-                Thread.Sleep(500);
-            }
+            RunServer();
 
             Console.WriteLine("Ended");
             Console.ReadKey();
@@ -72,8 +47,7 @@ namespace audioStream
             listener.Prefixes.Add("http://localhost:8080/");
             listener.Prefixes.Add("http://192.168.1.102:8080/");
             listener.Start();
-            var fs = new FileStream("S:\\a.wav", FileMode.Open);
-            Console.WriteLine(fs.Length);
+            Console.WriteLine("[HTTP] Server start");
             while (true)
             {
                 HttpListenerContext context = listener.GetContext();
@@ -86,47 +60,10 @@ namespace audioStream
                     response.ContentType = "audio/x-wav;codec=pcm";
                     response.SendChunked = true;
 
-
-                    //try
-                    //{
-                    //    fs.Seek(0, SeekOrigin.Begin);
-                    //    fs.CopyTo(response.OutputStream);
-                    //}
-                    //catch (Exception e)
-                    //{
-                    //    Console.WriteLine(e);
-                    //}
-                    var header = RiffHeader((int)audioBuf.Length).ToArray();
+                    var header = RiffHeader(0).ToArray();
                     response.OutputStream.Write(header, 0, header.Length);
-                    clientS = response.OutputStream;
-                    continue;
-                    while (true)
-                    {
-                        audioDataEvent.WaitOne();
-                        lock (audioStreamLock)
-                        {
-                            var buf = new byte[audioBuf.Length];
-                            audioBuf.GetBuffer().CopyTo(buf, 0);
-                            try
-                            {
-                                
-                                //Console.WriteLine("Header size: " + header.Length);
-                                //Console.WriteLine(BitConverter.ToString(header));
-
-                                
-                                response.OutputStream.Write(buf);
-                            }
-                            catch (Exception ex)
-                            {
-                                Console.WriteLine("[HTTP] Cannot write to client stream");
-                                Console.WriteLine(ex.Message);
-                                Console.WriteLine(ex.StackTrace);
-                                break;
-                            }
-                        }
-                    }
-                    try { response.OutputStream.Close(); }
-                    catch { }
+                    clientS.Add(response.OutputStream);
+                    Console.WriteLine("[HTTP] Added one client to stream");
                 }
                 else
                 {
@@ -142,25 +79,6 @@ namespace audioStream
                     response.OutputStream.Close();
                 }
             }
-        }
-
-        static List<byte> RiffHeader2(int size)
-        {
-            List<byte> header = new List<byte>();
-            header.AddRange(Encoding.ASCII.GetBytes("RIFF"));
-            header.AddRange(BitConverter.GetBytes(size + 36).Reverse().ToArray());
-            header.AddRange(Encoding.ASCII.GetBytes("WAVE"));
-            header.AddRange(Encoding.ASCII.GetBytes("fmt "));
-            header.AddRange(BitConverter.GetBytes(16).Reverse().ToArray());
-            header.AddRange(BitConverter.GetBytes((short)1).Reverse().ToArray());
-            header.AddRange(BitConverter.GetBytes((short)2).Reverse().ToArray());
-            header.AddRange(BitConverter.GetBytes(44100).Reverse().ToArray());
-            header.AddRange(BitConverter.GetBytes(176400).Reverse().ToArray());
-            header.AddRange(BitConverter.GetBytes((short)4).Reverse().ToArray());
-            header.AddRange(BitConverter.GetBytes((short)16).Reverse().ToArray());
-            header.AddRange(Encoding.ASCII.GetBytes("data"));
-            header.AddRange(BitConverter.GetBytes(size).Reverse().ToArray());
-            return header;
         }
 
         static List<byte> RiffHeader(int size)
